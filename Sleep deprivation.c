6760d1dc325baa3bb48b858904c61a0d9e3e9035
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 15:00:53 +0600

    Add files via upload

---
+++ Sleep deprivation.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main ()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n<7)
+            printf("YES\n");
+        else
+
+            printf("NO\n");
+    }
+    return 0;
+}
