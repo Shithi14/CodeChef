8d13aa5e9c01e66faec4dda424152a1f98fa506b
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:38:57 +0600

    Add files via upload

---
+++ 34.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int T,X,Y;
+    scanf("%d",&T);
+    while(T--)
+    {
+        scanf("%d%d",&X,&Y);
+        if(X<Y)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
