59bc903e19cb07fd76e322895c6c1a42a677d782
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:56:19 +0600

    Add files via upload

---
+++ MATH1 Enrolment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(a<b)
+            printf("%d\n",b-a);
+        else
+
+            printf("0\n");
+    }
+    return 0;
+}
