1312e95aea9b8858124be5987d954c41fd426df5
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:29:30 +0600

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int T,X,i,sum;
+    scanf("%d",&T);
+    for(i=1;i<=T;i++)
+    {
+        scanf("%d",&X);
+        sum=15*X;
+        printf("%d\n",sum);
+    }
+}
