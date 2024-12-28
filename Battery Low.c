4bfadc9b02700329e5691d3b18a940ee7a8dc8ce
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:47:53 +0600

    Add files via upload

---
+++ Battery Low.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n<=15)
+            printf("Yes\n");
+        else
+            printf("No\n");
+
+    }
+}
