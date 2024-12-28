3cc7fbe60462d1f5efc48d6cf77c081d3c900119
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:30:41 +0600

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int T,X,i,s;
+    scanf("%d",&T);
+    for(i=1;i<=T;i++)
+    {
+        scanf("%d",&X);
+        s=10*X;
+        printf("%d\n",s);
+    }
+}
