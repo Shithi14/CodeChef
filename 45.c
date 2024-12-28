036f8f13587fc2163c12deb567d26161afb3d9df
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:44:34 +0600

    Add files via upload

---
+++ 45.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main ()
+{
+    int T,K,X,s;
+    scanf("%d",&T);
+    while(T--)
+    {
+        scanf("%d%d",&K,&X);
+        s=(7*K-X);
+        printf("%d\n",s);
+
+
+    }
+}
