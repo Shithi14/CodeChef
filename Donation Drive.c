0794fc9d1ee8659d48e3ef23a7e2ad4000835021
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:52:20 +0600

    Add files via upload

---
+++ Donation Drive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,sum;
+        scanf("%d%d",&a,&b);
+        sum=a-b;
+        printf("%d\n",sum);
+    }
+    return 0;
+
+}
