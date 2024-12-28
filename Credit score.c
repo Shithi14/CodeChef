75156e3cf4ce7c1794c783d309d7361cdd8d8656
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:51:37 +0600

    Add files via upload

---
+++ Credit score.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main ()
+{
+
+
+          int a;
+            scanf("%d",&a);
+            if(a>=750)
+    printf("YES\n");
+    else
+        printf("NO\n");
+
+          }
+
+
