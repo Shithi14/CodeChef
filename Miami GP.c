15586b802e0601320688798efe7b16fd8cdf21ff
Shithi Rani Roy <cse12105009brur@gmail.com>
2024-12-28 14:57:05 +0600

    Add files via upload

---
+++ Miami GP.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main ()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,s;
+        scanf("%d%d",&a,&b);
+        s=a*1.07;
+        if(s>=b)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+}
