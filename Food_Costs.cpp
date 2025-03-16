b313a30671c95a63eb72cbbb75d86477ace0a676
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:47:21 +0600

    Add files via upload

---
+++ Food_Costs.cpp
@@ -0,0 +1,18 @@
+
+//https://www.codechef.com/problems/FOODCOST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+   int m,n;
+   cin>>m>>n;
+   cout<<6*m+n<<endl;
+
+
+return 0;
+}
