d1c1f1b6a95c83c3552c09b30c9b7534ce3c862c
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:47:59 +0600

    Add files via upload

---
+++ Bone_Appetit.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/BNE_APT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+    int n,m,x,z;
+    cin>>n>>m>>x>>z;
+    int nx=n*x;
+    int mz=m*z;
+    cout<<nx+mz<<endl;
+   
+
+return 0;
+}
