b1782cdbabade6e2c8e5ca1a4629e122cda9a6a6
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:48:10 +0600

    Add files via upload

---
+++ 2000.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/RIP2000
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+   int a;
+   cin>>a;
+   cout<<a*4<<endl;
+
+return 0;
+}
