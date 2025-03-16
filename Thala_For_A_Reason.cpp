75d0e9c6cc847f434f501c91df6c0798a22e6040
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:46:40 +0600

    Add files via upload

---
+++ Thala_For_A_Reason.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+    int n;
+    cin>>n;
+    if (n==7){
+        cout<<"THALA"<<endl;
+    }
+   else{
+    cout<<"SADGE"<<endl;
+   }
+
+return 0;
+}
