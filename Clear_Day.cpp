a559358b6683f50ff63cbf8552dd8da4bd385795
Shithi Rani Roy <cse12105009brur@gmail.com>
2025-03-16 23:47:47 +0600

    Add files via upload

---
+++ Clear_Day.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+int main()
+{
+   int a,b;
+   cin>>a>>b;
+   int clear=7-(a+b);
+   cout<<clear<<endl;
+
+return 0;
+}
